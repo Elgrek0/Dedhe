3dfafee93389cc8a3d42f4c73e2e98b6c53dc313
Paris Stefanou <gaspar093@hotmail.com>
2015-11-29 11:34:25 +0200

    h2 jar

---
+++ src/Gui/LoadDataFromDb.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Gui;
+
+/**
+ *
+ * @author Paris
+ */
+public class LoadDataFromDb implements LoadDataFromDBInterface {
+
+    @Override
+    public String[] getalltransformers() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+  
+    
+}
