c5b3305cc13b5356435e7fd47270b1370c522f68
Paris Stefanou <gaspar093@hotmail.com>
2016-02-24 07:54:11 +0200

    Nomizw mas kanei auto gia na blepoun ta values

--- src/panels/Analytics/GraphPanel.java
+++ src/panels/Analytics/GraphPanel.java
@@ -1,24 +1,36 @@
 package panels.Analytics;
 
 import DB_data_loader.data_classes.ElectricalValue;
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.geom.Rectangle2D;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Vector;
 import javax.swing.JPanel;
 import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartMouseEvent;
+import org.jfree.chart.ChartMouseListener;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.labels.StandardCrosshairLabelGenerator;
+import org.jfree.chart.panel.CrosshairOverlay;
+import org.jfree.chart.plot.Crosshair;
 import org.jfree.chart.plot.Plot;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYItemRenderer;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.chart.renderer.xy.XYSplineRenderer;
+import org.jfree.data.general.DatasetUtilities;
+import org.jfree.data.time.Day;
 import org.jfree.data.time.Minute;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
 import org.jfree.data.time.Year;
 import org.jfree.data.xy.XYDataset;
+import org.jfree.ui.RectangleEdge;
 import org.jfree.ui.RectangleInsets;
 
 /**
@@ -26,12 +38,72 @@ import org.jfree.ui.RectangleInsets;
  * used, except that the renderer is modified to show filled shapes (as well as
  * lines) at each data point.
  */
-public class GraphPanel extends JPanel {
+public class GraphPanel extends JPanel implements ChartMouseListener {
 
-    public ChartPanel panel;
+    public ChartPanel chartpanel;
+    private Crosshair xCrosshair;
+    private Crosshair yCrosshair;
 
     public GraphPanel(Vector<ElectricalValue> values) {
 
+        TimeSeriesCollection dataset = createDataset(values);
+
+        add(createContent(dataset));
+        setSize(800, 500);
+        setVisible(true);
+
+    }
+
+    private static JFreeChart createChart(XYDataset dataset) {
+
+        JFreeChart chart = ChartFactory.createTimeSeriesChart(
+                "", // title
+                "Date", // x-axis label
+                "Current", // y-axis label
+                dataset, // data
+                true, // create legend?
+                true, // generate tooltips?
+                false // generate URLs?
+        );
+
+        chart.setBackgroundPaint(Color.white);
+
+        XYPlot plot = (XYPlot) chart.getPlot();
+        plot.setBackgroundPaint(Color.lightGray);
+        plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinePaint(Color.white);
+        plot.setAxisOffset(new RectangleInsets(5.0, 5.0, 5.0, 5.0));
+        plot.setDomainCrosshairVisible(true);
+        plot.setRangeCrosshairVisible(true);
+
+        XYSplineRenderer renderer = new XYSplineRenderer(5);
+        plot.setRenderer(renderer);
+
+        renderer.setBaseShapesVisible(false);
+
+        DateAxis axis = (DateAxis) plot.getDomainAxis();
+        axis.setDateFormatOverride(new SimpleDateFormat("HH-mm MM-dd"));
+
+        return chart;
+
+    }
+
+    private JPanel createContent(TimeSeriesCollection dataset) {
+        JFreeChart chart = createChart(dataset);
+        chartpanel = new ChartPanel(chart);
+        chartpanel.addChartMouseListener(this);
+        CrosshairOverlay crosshairOverlay = new CrosshairOverlay();
+        xCrosshair = new Crosshair(Double.NaN, Color.GRAY, new BasicStroke(0f));
+        xCrosshair.setLabelVisible(false);
+        yCrosshair = new Crosshair(Double.NaN, Color.GRAY, new BasicStroke(0f));
+        yCrosshair.setLabelVisible(true);
+        crosshairOverlay.addDomainCrosshair(xCrosshair);
+        crosshairOverlay.addRangeCrosshair(yCrosshair);
+        chartpanel.addOverlay(crosshairOverlay);
+        return chartpanel;
+    }
+
+    private TimeSeriesCollection createDataset(Vector<ElectricalValue> values) {
         TimeSeries s1 = new TimeSeries("Current of powerstation_name");
 
         for (int i = 0; i < values.size(); i++) {
@@ -41,50 +113,26 @@ public class GraphPanel extends JPanel {
 
         TimeSeriesCollection dataset = new TimeSeriesCollection(s1);
 
-        JFreeChart chart = createChart(dataset);
-
-        Plot plot = chart.getPlot();
-
-        panel = new ChartPanel(chart);
-        panel.setFillZoomRectangle(false);
-        panel.setMouseWheelEnabled(true);
-        add(panel);
-        setSize(800, 500);
-        setVisible(true);
-
+        return dataset;
     }
 
-    private static JFreeChart createChart(XYDataset dataset) {
+    @Override
+    public void chartMouseClicked(ChartMouseEvent event) {
+        // ignore
+    }
 
-        JFreeChart chart = ChartFactory.createTimeSeriesChart(
-                "data on date", // title
-                "Date", // x-axis label
-                "Current", // y-axis label
-                dataset, // data
-                false, // create legend?
-                false, // generate tooltips?
-                false // generate URLs?
-        );
-
-        chart.setBackgroundPaint(Color.white);
-
+    @Override
+    public void chartMouseMoved(ChartMouseEvent event) {
+        Rectangle2D dataArea = chartpanel.getScreenDataArea();
+        JFreeChart chart = event.getChart();
         XYPlot plot = (XYPlot) chart.getPlot();
-        //plot.setBackgroundPaint(Color.lightGray);
-        //plot.setDomainGridlinePaint(Color.white);
-        //plot.setRangeGridlinePaint(Color.white);
-        //plot.setAxisOffset(new RectangleInsets(5.0, 5.0, 5.0, 5.0));
-        //plot.setDomainCrosshairVisible(true);
-        //plot.setRangeCrosshairVisible(true);
-
-        XYSplineRenderer renderer = new XYSplineRenderer(5);
-        plot.setRenderer(renderer);
-        renderer.setBaseShapesVisible(false);
-
-        DateAxis axis = (DateAxis) plot.getDomainAxis();
-        axis.setDateFormatOverride(new SimpleDateFormat("HH-mm MM-dd"));
-
-        return chart;
-
+        ValueAxis xAxis = plot.getDomainAxis();
+        double x = xAxis.java2DToValue(event.getTrigger().getX(), dataArea,
+                RectangleEdge.BOTTOM);
+        double y = DatasetUtilities.findYValue(plot.getDataset(), 0, x);
+        
+        xCrosshair.setValue(x);
+        yCrosshair.setValue(y);
     }
 
 }
